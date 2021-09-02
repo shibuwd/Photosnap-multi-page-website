d0ce9884856b3d871eae20ca457f7d49928ab396
Shibu Sarkar <shibusarkar619@gmail.com>
2021-09-03 00:15:48 +0600

    add media query for tablet

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
-		header.style.backgroundColor = '#fbfcfe';
+		header.style.backgroundColor = '#fff';
 } else {
 		header.style.backgroundColor = 'transparent';
 	}
